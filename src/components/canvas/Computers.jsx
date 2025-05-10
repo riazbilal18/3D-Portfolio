cc6d07ffe5c83d7941e7535d26d93c52da8e9590
Bilal Riaz <riazbilal18@gmail.com>
2025-05-09 22:05:10 -0400

    removed unused import

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,7 +1,6 @@
 import { Suspense, useEffect, useState } from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
-import CanvasLoader from "../Loader";
 
 const Computers = () => {
   const computer = useGLTF("./desktop_pc/scene.gltf");
