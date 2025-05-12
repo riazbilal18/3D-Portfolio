62aa8cec76d435752f5b39d2e0b6c7a0fafa1340
Bilal Riaz <riazbilal18@gmail.com>
2025-05-12 13:58:11 -0400

    added tech stack names

--- src/constants/index.js
+++ src/constants/index.js
@@ -89,15 +89,15 @@ import {
       icon: mongodb,
     },
     {
-      name: "git",
+      name: "Git",
       icon: git,
     },
     {
-      name: "figma",
+      name: "Figma",
       icon: figma,
     },
     {
-      name: "docker",
+      name: "Docker",
       icon: docker,
     },
   ];
